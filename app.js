5d8fb8e94ee9a5a1c9ac91bfbae08450948ba551
Jeffery Sloan <jefferysloanjr@gmail.com>
2017-11-05 20:41:49 -0800

    fixing merge issue

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var lastImage = [1, 2, 3];
 var currentImages = [4, 5, 6];
+<<<<<<< HEAD
 
 var allImages = [];
 
@@ -15,18 +16,36 @@ var reset = document.getElementById('rst-btn');
 
 var twentyFive = 25;
 
+=======
+var allImages = [];
+var imageSet = [
+  document.getElementById('first-set'),
+  document.getElementById('second-set'),
+  document.getElementById('third-set')
+];
+
+var twentyFive = 25;
+
+>>>>>>> adjusted code added chart
 var objImages = [];
 var objShow = [];
 var objClick = [];
 
+<<<<<<< HEAD
 var Images = function (mame, file) {
   this.name = name;
   this.filePath = './bm-img/' + name + ',' + file;
+=======
+var images = function (mame, file) {
+  this.name = name;
+  this.filePath = './img/' + name + ',' + file;
+>>>>>>> adjusted code added chart
   this.valid = true;
   this.shown = 0;
   this.timesClicked = 0;
 };
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 // all images to be called
 imageSet.push(
@@ -53,12 +72,40 @@ imageSet.push(
 );
 
 Images.prototype.imgTag = function () {
+=======
+// all images to be called
+imageSet.push(
+  new Image('bag', 'jpg'),
+  new Image('banana', 'jpg'),
+  new Image('bathroom', 'jpg'),
+  new Image('boots', 'jpg'),
+  new Image('breakfast', 'jpg'),
+  new Image('bubblegum', 'jpg'),
+  new Image('chair', 'jpg'),
+  new Image('cthulhu', 'jpg'),
+  new Image('dog-duck', 'jpg'),
+  new Image('dragon', 'jpg'),
+  new Image('pen', 'jpg'),
+  new Image('pet-sweep', 'jpg'),
+  new Image('scissors', 'jpg'),
+  new Image('shark', 'jpg'),
+  new Image('sweep', 'png'),
+  new Image('tauntaun', 'jpg'),
+  new Image('unicorn', 'jpg'),
+  new Image('usb', 'gif'),
+  new Image('water-can', 'jpg'),
+  new Image('wine-glass', 'jpg')
+);
+
+Image.prototype.imgTag = function () {
+>>>>>>> adjusted code added chart
   return '<img id="' + this.name + '" src="' + this.filePath + '" >';
 };
 
 var random = function () {
   return Math.floor(Math.random() * imageSet.length);
 };
+<<<<<<< HEAD
 
 // loop for next set of images
 var nextImage = function () {
@@ -251,3 +298,125 @@ function resetData (event) {
 console.log('click', oneClick);
 console.log('click', twoClick);
 console.log('click', threeClick);
+=======
+
+var nextImage = function () {
+  for (var i = 0; i < imageSet.length; i++) {
+    imageOne(lastImage[i]).valid = true;
+    lastImage[i] = currentImages[i];
+    while (imageOne(currentImage[i]).valid === false) {
+      currentImages[i] = random();
+    }
+    imageOne(currentImage[i]).valid = false;
+  }
+};
+
+var imageOne = function (list) {
+  return allImages[list];
+};
+
+var newSet = function () {
+  for (var m = 0; m < imageOne.length; m++) {
+    imageSet[m].innerHTML = '';
+    imageSet[m].innerHTML = imageOne(currentImages[k]).imgTag();
+  }
+};
+
+var newShown = function () {
+  for (var k = 0; 1 < 3; k++) {
+    imageSet(currentImages[k]).timesShown++;
+    imageSet(currentImages[k]).valid = false;
+  }
+};
+
+var oldSet = function () {
+  imageSet[0].removeEventListener('click', clickOne);
+  imageSet[1].removeEventListener('click', clickTwo);
+  imageSet[2].removeEventListenter('click', clickThree);
+  for (var j = 0; j < imageSet.length; j++) {
+    imageSet[m].style.visibility = 'hidden';
+  }
+};
+
+var data = function () {
+  for (var o = 0; o < allImages.length; o++) {
+    objImages.push(allImages[o].name);
+    objShow.push(allImages[o].shown);
+    objClick.push(allImages[o].clicks);
+  }
+};
+
+// Chart start
+var makeChart = function () {
+  var ctx = document.getElementById('dataChart').getContext('2d');
+  ctx.canvas.width = '1000';
+  ctx.canvas.height = '250';
+  var chart = new Chart(ctx, {
+    type: 'bar',
+
+    data: {
+      labels: objImages,
+      datasets: [{
+        label: 'Times Seen',
+        backgroundColor: '#f2f2f2',
+        borderColor: '#000',
+        data: objShow,
+      },
+      {
+        label: 'Times Clicked',
+        backgroundColor: '#017359',
+        borderColor: '#000',
+        data: objectClick,
+      }]
+    },
+
+    options: {
+      scales: {
+        xAxes: [{
+          stacked: true
+        }],
+        yAxes: [{
+          stacked: true
+        }]
+      }
+    }
+  });
+};
+
+// function to reset page
+var resetPage = function () {
+  newShown();
+  nextImage();
+  newSet();
+};
+
+resetPage();
+
+imageSet[0].addEventListener('click', oneClick);
+imageSet[1].addEventListener('click', twoClick);
+imageSet[2].addEventListener('click', threeClick);
+
+function oneClick (event) {
+  if (twentyFive > 0) {
+    imageSet(currentImages[1]).timesClicked++;
+    refresh();
+    twentyFive--;
+  } else {
+    oldSet();
+    data();
+    makeChart();
+  }
+}
+
+function threeClick (event) {
+  if (twentyFive > 0) {
+    imageSet(currentImages[2]).timesClicked++;
+    refresh();
+    twentyFive--;
+  } else {
+    oldSet();
+    data();
+    makeChart();
+  }
+}
+>>>>>>> adjusted code added chart
